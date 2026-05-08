a5b8021d1e290a0b3a43496b859917de6b46ea53
art0007i <art0007i@gmail.com>
2026-05-08 21:30:12 +0200

    minor cleanup

--- SqueezeEmIn/SqueezeEmIn.cs
+++ SqueezeEmIn/SqueezeEmIn.cs
@@ -19,10 +19,10 @@ public class SqueezeEmIn : ResoniteMod {
 	public static ModConfiguration config;
 
 	[AutoRegisterConfigKey]
-	private static ModConfigurationKey<bool> Enabled = new ModConfigurationKey<bool>("Enabled", "Enables the mod.", () => true);
+	private static ModConfigurationKey<bool> Enabled = new("Enabled", "Enables the mod.", () => true);
 
 	[AutoRegisterConfigKey]
-	private static ModConfigurationKey<string> UserIDsCommaSeperated = new ModConfigurationKey<string>("User IDs", "A comma-seperated list of User IDs to always let into your sessions, regardless of user cap.", () => "");
+	private static ModConfigurationKey<string> UserIDsCommaSeperated = new("User IDs", "A comma-seperated list of User IDs to always let into your sessions, regardless of user cap.", () => "");
 
 	[AutoRegisterConfigKey]
 	private static ModConfigurationKey<List<string>> UserIDsArray = new(
@@ -64,17 +64,11 @@ public class SqueezeEmIn : ResoniteMod {
 		return true;
 	}
 
-	[HarmonyPatch]
+	[HarmonyPatch(typeof(World), nameof(World.VerifyJoinRequest), MethodType.Async)]
 	public class World_VerifyJoinRequest_Patch {
-		[HarmonyTargetMethod]
-		static MethodBase CalculateMethod() {
-			var method = typeof(World).GetMethod(nameof(World.VerifyJoinRequest), BindingFlags.Instance | BindingFlags.NonPublic | BindingFlags.Public);
-			var asyncAttr = method.GetCustomAttribute<AsyncStateMachineAttribute>();
-			return asyncAttr.StateMachineType.GetMethod("MoveNext", BindingFlags.Instance | BindingFlags.NonPublic);
-		}
-
-		[HarmonyTranspiler]
 		static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions, MethodBase originalMethod) {
+			var lookFor = AccessTools.PropertyGetter(typeof(World), nameof(World.HasFreeUserSpots));
+			
 			var customMethod = AccessTools.Method(typeof(World_VerifyJoinRequest_Patch), nameof(CustomHasFreeUserSpotsCheck));
 
 			var stateMachineType = originalMethod.DeclaringType;
@@ -83,32 +77,29 @@ public class SqueezeEmIn : ResoniteMod {
 			bool patched = false;
 
 			foreach (var instruction in instructions) {
-				// 1. Is it a Call or Callvirt?
-				// 2. Is the operand a MethodInfo?
-				// 3. Is the name of the method exactly what we are looking for?
-				bool isTargetMethod = (instruction.opcode == OpCodes.Call || instruction.opcode == OpCodes.Callvirt) &&
-									  (instruction.operand is MethodInfo method && method.Name == "get_HasFreeUserSpots");
+				// Does the instruction call the function we want to replace?
+				bool isTargetMethod = instruction.Calls(lookFor);
 
 				if (!patched && isTargetMethod) {
 					// 1. The original code JUST pushed 'World' onto the stack.
 					// Stack: [ ..., World ]
 
 					// 2. Push the state machine instance ('this') onto the stack.
-					yield return new CodeInstruction(OpCodes.Ldarg_0);
+					yield return new(OpCodes.Ldarg_0);
 					// Stack: [ ..., World, StateMachineInstance ]
 
 					// 3. Load the 'connection' field from the state machine instance.
-					yield return new CodeInstruction(OpCodes.Ldfld, connectionField);
+					yield return new(OpCodes.Ldfld, connectionField);
 					// Stack: [ ..., World, SessionConnection ]
 
 					// 4. Call our custom method! It consumes BOTH arguments and returns a bool, 
 					// keeping the IL stack perfectly balanced for the upcoming branch instruction.
-					yield return new CodeInstruction(OpCodes.Call, customMethod);
+					yield return new(OpCodes.Call, customMethod);
 
 					patched = true;
 					Msg("Ready to squeeze 'em in!");
 				} else {
-					// this isnt what we're looking for... keep walking bro
+					// this isn't what we're looking for... keep walking bro
 					yield return instruction;
 				}
 			}
